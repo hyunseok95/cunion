c24f21d16b99724bce03ed5ea806c53542360df4
hyunseok95 <hyunseok095@gmail.com>
2023-04-10 18:27:20 +0900

    임포트 구조 변경

--- main.go
+++ main.go
@@ -1,20 +1,17 @@
 package main
 
 import (
-	"crypto"
 	"math/rand"
 	"os"
 	"time"
 
 	"fmt"
 
-	"github.com/containerd/containerd/pkg/hasher"
 	"github.com/hyunseok95/cunion/cmd"
 )
 
 func init() {
 	rand.NewSource(time.Now().UnixNano())
-	crypto.RegisterHash(crypto.SHA256, hasher.NewSHA256)
 }
 
 func main() {
@@ -27,8 +24,6 @@ func main() {
     fmt.Println("Executing command:", command)
 
     switch command {
-    case "containerd":
-        cmd.Containerd()
     case "test":
         cmd.Test()
     default:
